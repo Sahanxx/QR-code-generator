a37993fda5a3b5b8c8aa0cc6b10484565b59e62c
Sahan Anjana Hewage <sahananjana63@gmail.com>
2025-09-15 15:53:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/QR-code-generator/', // IMPORTANT for project pages
+  base: '/QR-code-generator/', // must match repo name (case sensitive)
   plugins: [react()],
 })
